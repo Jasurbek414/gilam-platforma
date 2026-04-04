8ea4491c2adbf04183f39d66a01b774e29693f77
Jasurbek414 <muminovjasurbek12@gmail.com>
2026-04-04 12:25:28 +0500

    Fix: Wrap OperatorLayout with ChatProvider to enable global useChat hook

--- frontend-app/src/app/operator/layout.tsx
+++ frontend-app/src/app/operator/layout.tsx
@@ -1,5 +1,6 @@
 import OperatorSidebar from '@/components/layout/OperatorSidebar';
 import Topbar from '@/components/layout/Topbar';
+import { ChatProvider } from '@/context/ChatContext';
 
 export default function OperatorLayout({
   children,
@@ -7,18 +8,20 @@ export default function OperatorLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex bg-slate-50 min-h-screen">
-      <OperatorSidebar />
+    <ChatProvider>
+      <div className="flex bg-slate-50 min-h-screen">
+        <OperatorSidebar />
 
-      <div className="flex-1 ml-72 flex flex-col min-h-screen">
-        <Topbar />
-        
-        <main className="flex-1 p-3 lg:p-4 overflow-auto bg-slate-50/50">
-          <div className="w-full h-full">
-            {children}
-          </div>
-        </main>
+        <div className="flex-1 ml-72 flex flex-col min-h-screen">
+          <Topbar />
+          
+          <main className="flex-1 p-3 lg:p-4 overflow-auto bg-slate-50/50">
+            <div className="w-full h-full">
+              {children}
+            </div>
+          </main>
+        </div>
       </div>
-    </div>
+    </ChatProvider>
   );
 }
